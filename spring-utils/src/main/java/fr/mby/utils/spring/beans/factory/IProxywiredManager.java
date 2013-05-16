85be50149432fe1cecc352967acf3543da13aa10
mxbossard <mxbossard@gmail.com>
2013-05-16 08:25:47 +0200

    Adding configuration capabilities to Proxywired annotation.

--- spring-utils/src/main/java/fr/mby/utils/spring/beans/factory/IProxywiredManager.java
+++ spring-utils/src/main/java/fr/mby/utils/spring/beans/factory/IProxywiredManager.java
@@ -23,24 +23,69 @@ import java.util.Set;
 import org.springframework.beans.factory.config.DependencyDescriptor;
 
 /**
+ * Manager for @Proxywired dependencies.
+ * 
  * @author Maxime BOSSARD.
  * 
  */
 public interface IProxywiredManager {
 
+	/**
+	 * Retrieve a Proxywired dependency for injection.
+	 * 
+	 * @param descriptor
+	 * @param beanName
+	 * @param autowiredBeanNames
+	 * @param target
+	 * @return
+	 */
 	Object getProxywiredDependency(DependencyDescriptor descriptor, String beanName, Set<String> autowiredBeanNames,
 			Object target);
 
+	/**
+	 * Modify a Proxywired dependency.
+	 * 
+	 * @param proxywiredKey
+	 * @param beanNames
+	 */
 	void modifyProxywiredDepencies(String proxywiredKey, LinkedHashSet<String> beanNames);
 
+	/**
+	 * Ordered view of all bean names currently in one Proxywired dependency.
+	 * 
+	 * @param proxywiredKey
+	 * @return
+	 */
 	Set<String> viewProxywiredDependencies(String proxywiredKey);
 
+	/**
+	 * View of all bean names of a specific type currently laying in the BeanFactory.
+	 * 
+	 * @param type
+	 * @return
+	 */
 	Set<String> viewAllDependencies(Class<?> type);
 
+	/**
+	 * Represent a Manageable Proxywired dependency.
+	 * 
+	 * @author Maxime Bossard - 2013.
+	 * 
+	 */
 	public interface IProxywiredManageable {
 
+		/**
+		 * Modify the elements inside the Proxywired dependency.
+		 * 
+		 * @param dependencies
+		 */
 		void modifyProxywiredDependencies(LinkedHashMap<String, Object> dependencies);
 
+		/**
+		 * Oredered view currently wired in de Proxywired dependency.
+		 * 
+		 * @return
+		 */
 		Set<String> viewProxywiredDependencies();
 
 	}
